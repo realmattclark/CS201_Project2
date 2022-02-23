0bd3c7e7cac9468ead7bd790f91f755dc128e7bb
realmattclark <54007279+realmattclark@users.noreply.github.com>
2022-02-22 21:30:03 -0600

    Update master/source

--- CS201_Project2.cpp
+++ CS201_Project2.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int liveCards[16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+int liveCards[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
 int deadCards[16] = {};
 int score = 0;
-int through() {
+
+int isOver() {
 	int total = 0;
 	for (int i = 0; i < 16; i++) {
 		if (deadCards[i] > 0) total++;
 	}
-	return total;
+	return total == 16 ? 0 : 1;
 }
 
 void showDeadCards() {
+
 	for (int i = 0; i < 16; i++) {
 		if (deadCards[i] > 0)
-		cout << right << deadCards[i];
+			cout << deadCards[i];
 	}
+
 	cout << endl;
+
 }
 
 void showLiveCards() {
+
 	for (int i = 0; i < 16; i++) {
 		if (liveCards[i] > 0)
-			cout << std::right << liveCards[i];
-	};
+			cout << liveCards[i];
+	}
 	cout << endl;
 }
 
@@ -32,18 +39,11 @@ void showLiveCards() {
 int isDead(int num) {
 
 	for (int i = 0; i < 16; i++) {
-
 		if (deadCards[i] == num)
-
 			return 1;
-
 	}
-
 	return 0;
-
 }
-
-
 
 int nextCard() {
 
@@ -51,7 +51,7 @@ int nextCard() {
 
 	if (isDead(num)) {
 
-		cout << num << "Dead" << endl;
+		cout << num << "Dead: " << endl;
 
 		return 0;
 
@@ -66,7 +66,7 @@ int nextCard() {
 
 }
 
-void takeIt(int card) {
+void takeit(int card) {
 
 	score += card;
 
@@ -80,7 +80,7 @@ void takeIt(int card) {
 
 }
 
-void leave(int card) {
+void leaveit(int card) {
 
 	deadCards[card - 1] = card;
 
@@ -89,5 +89,55 @@ void leave(int card) {
 }
 
 int main() {
-	cout << "Hey twitta worl" << endl;
+
+	char choice[10];
+
+	int card;
+
+	while (true) {
+
+		while (isOver()) {
+
+			cout << "Current Score: " << score << endl;
+
+			cout << "Dead cards:";
+
+			showDeadCards();
+
+			cout << "Live cards: ";
+
+			showLiveCards();
+
+			cout << "Next cards: ";
+
+			card = nextCard();
+
+			if (card != 0) {
+
+				cout << "Take it [T] or Leave it [L]?";
+
+				cin >> choice;
+
+				if (choice[0] == 'T' || choice[0] == 't') {
+
+					takeit(card);
+
+				}
+				else {
+
+					leaveit(card);
+				}
+			}
+		}
+		cout << "Want to play Again - Yes [Y] No [N] ?";
+
+		cin >> choice;
+
+		if (choice[0] == 'N' || choice[0] == 'n') {
+
+			break;
+
+		}
+	}
+	return 0;
 }
